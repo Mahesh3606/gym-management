9e5776c35baf5750d7411fabfd27044ce924946c
Mahesh3606 <lettertomahesh2006@gmail.com>
2025-07-22 14:20:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let menu_icon = document.getElementById('menu-icon');
+let menu = document.querySelector('.nav-list');
+
+function showmenu(){
+    menu.style.display = 'block';
+}
+
+function hidemenu(){
+    menu.style.display = 'none';
+}
